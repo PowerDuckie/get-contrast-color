d8b751b3078b30d467c6ece43885e00eaa0be40e
phpwechat <3257132998@qq.com>
2025-11-25 04:22:45 +0800

    feat: modify readme

--- demo.js
+++ demo.js
@@ -6,3 +6,13 @@ console.log(getContrastColor('rgb(255,0,0,0.5)')); // -> "#ffffff"
 console.log(getContrastColor('linear-gradient(135deg, #2563EB 0%, #9C27B0 100%)')); // -> "#ffffff"
 console.log(getContrastColor('linear-gradient(to right, rgba(255,0,0,0.5), hsla(240,100%,50%,0.5))')); // "#ffffff"
 console.log(getContrastColor('linear-gradient(135deg, #f36a0fff 50%, #c7d8f3ff 50%)')); // "#00000"
+
+
+// Solid color examples
+console.log(getContrastColor('#ff0000')); // -> "#ffffff"
+console.log(getContrastColor('rgb(240,240,240)')); // -> "#000000"
+console.log(getContrastColor('hsl(120, 100%, 50%)')); // -> "#000000"
+
+// Linear gradient examples
+console.log(getContrastColor('linear-gradient(135deg, #2563EB 0%, #9C27B0 100%)')); // -> "#ffffff"
+console.log(getContrastColor('linear-gradient(to right, rgba(255,0,0,0.5), hsla(240,100%,50%,0.5))')); // -> "#ffffff"
